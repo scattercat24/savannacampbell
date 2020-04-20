98fe4316ca182fb51a9eb34658b013e7660fbe49
scattercat24 <63881728+scattercat24@users.noreply.github.com>
2020-04-20 19:20:30 -0400

    New Update Door animation added
    Added house prefabs, a door animation, and attempted(and failed) at making player controllers.

---
+++ A Broken Block/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+  public float speedH;
+  public float speedV;
+  public float speed;
+
+  private Rigidbody rb;
+  private float yaw = 0.0f;
+  private float pitch = 0.0f;
+
+  void Start (){
+    rb = GetComponent<Rigidbody>();
+  }
+  void FixedUpdate () {
+    Move();
+
+    // float vertical = Input.GetAxis("Vertical");
+    // float horizontal = Input.GetAxis("Horizontal");
+    // Vector3 vel = rb.velocity;
+    // vel.z = vertical;
+    // vel.x = horizontal;
+    // rb.velocity = vel * speed;
+
+    yaw += speedH * Input.GetAxis("Mouse X");
+    pitch -= speedV * Input.GetAxis("Mouse Y");
+
+
+    transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+
+  }
+  void Move () {
+    if (Input.GetKeyDown(KeyCode.UpArrow)) {
+      rb.AddRelativeForce(Vector3.forward * speed);
+    }
+  }
+
+}
